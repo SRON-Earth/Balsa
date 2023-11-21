b6ee0d0a0334ef3f02cc168e36c78e3d25233852
Denis de Leeuw Duarte <denis@jigsaw.nl>
2023-11-21 10:10:44 +0100

    * Added comments to ingestion.h

--- Sources/ingestion.h
+++ Sources/ingestion.h
@@ -22,32 +22,39 @@ T read(std::istream &stream)
 
 TrainingDataSet::SharedPointer loadTrainingDataSet(const std::string &filename)
 {
+    // Open the file stream.
     std::ifstream stream(filename, std::ios::binary);
     assert(stream.good());
 
+    // Read the column count of the input matrix.
     const unsigned int tupleSize = read<std::uint32_t>(stream);
     const unsigned int featureCount = tupleSize - 1;
 
+    // Read the format header string, with type info for each column.
     std::string formatString(tupleSize, '?');
     stream.read(formatString.data(), formatString.size());
     for (unsigned i = 0; i < featureCount; ++i)
         assert(formatString.at(i) == 'f');
     assert(formatString.back() == 'B');
 
+    // Read the rows of points.
     TrainingDataSet::SharedPointer dataset(new TrainingDataSet(featureCount));
     while (stream.good())
     {
+        // Read the data point columns.
         DataPoint point(featureCount);
         for (unsigned int i = 0; i < featureCount; ++i)
         {
             point.at(i) = read<float>(stream);
         }
 
+        // Read the label.
         const bool label = (read<std::uint8_t>(stream) != 0);
 
         if (stream.eof())
             break;
 
+        // Add the point to the dataset.
         dataset->appendDataPoint(point, label);
     }
 
