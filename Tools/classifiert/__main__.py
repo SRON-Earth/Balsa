9ec5f479c21d62c5f21687557115ef886d78a3f7
Joris van Zwieten <joris@jigsaw.nl>
2023-12-06 16:31:23 +0100

    Update exception error message.

--- Tools/classifiert/__main__.py
+++ Tools/classifiert/__main__.py
@@ -5,5 +5,5 @@ from .app import main
 try:
     sys.exit(main())
 except Exception as exception:
-    print(f"ERROR: '{exception}'.", file=sys.stderr)
+    print(f"INTERNAL ERROR: '{exception}'.", file=sys.stderr)
     sys.exit(1)
